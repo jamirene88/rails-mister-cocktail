0f3c9953729166253072e07ab9286dd4a99d6984
Jami Owen <jamirene88@gmail.com>
2018-11-30 09:17:29 +0000

    created controllers for cocktails and doses

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+require 'open-uri'
+require 'byebug'
+
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+
+puts "creating cocktails"
+url = 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Cocktail'
+cocktails_page = open(url).read
+cocktails = JSON.parse(cocktails_page)
+
+cocktails['drinks'].each do |drink|
+  Cocktail.create(name: drink["strDrink"])
+
+end
+puts "created #{cocktails.length} cocktails"
